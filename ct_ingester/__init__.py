974e8732c380c3260a4fb67337727771e9b81948
Adamos Kyriakou <somada141@gmail.com>
2017-11-21 22:28:42 +1100

    Added boilerplate modules for configuration, logging, custom-exception classes, as well as base DAL and ORM classes.

--- ct_ingester/__init__.py
+++ ct_ingester/__init__.py
@@ -5,3 +5,9 @@
 __author__ = """Adamos Kyriakou"""
 __email__ = 'somada141@gmail.com'
 __version__ = '0.1.0'
+
+from ct_ingester import config
+from ct_ingester import dal_base
+from ct_ingester import excs
+from ct_ingester import loggers
+from ct_ingester import orm_base
