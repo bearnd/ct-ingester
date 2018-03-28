8fc529fc7df67a5d19ba446748912a7edfd335d5
Adamos Kyriakou <somada141@gmail.com>
2018-03-28 11:49:21 +1100

    `utils.py`: Fixed bugs in the `EnumBase` class.

--- ct_ingester/utils.py
+++ ct_ingester/utils.py
@@ -6,14 +6,29 @@ import enum
 class EnumBase(enum.Enum):
     """Enumeration base-class."""
 
-    @staticmethod
-    def get_enum(value):
+    @classmethod
+    def get_member(
+        cls,
+        value: str,
+    ):
+        """Returns an enumeration member with a value matching `value`.
+
+        Args:
+            value (str): The value of the member to match.
+
+        Returns:
+            The matching member of `None` if `value` is undefined or no member
+                was found.
+        """
         if not value:
             return None
 
         members = [
-            (member, member.value) for member in ArticlePubModel
+            (member, member.value)
+            for member in cls.__members__.values()
         ]
         for member, member_value in members:
-            if member_value.lower() == value.lower():
+            if member_value == value:
                 return member
+
+        return None
