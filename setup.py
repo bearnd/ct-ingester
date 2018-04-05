3645893d5d25dbd06702fe0939a5afbc6ec33b66
Adamos Kyriakou <somada141@gmail.com>
2018-04-05 13:12:56 +1000

    `setup.py`: Fixed issue where a chunk of the file including the imports was missing.

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
