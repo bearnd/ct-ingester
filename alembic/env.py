2447955dc7c7f4351f166a9d8cef414da5698254
Adamos Kyriakou <somada141@gmail.com>
2018-04-08 21:52:48 +1000

    `env.py`: Updated the `run_migrations_online` function to account for the schema name.

--- alembic/env.py
+++ alembic/env.py
@@ -86,7 +86,8 @@ def run_migrations_online():
     with engine.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
+            target_metadata=target_metadata,
+            version_table_schema=target_metadata.schema,
         )
 
         with context.begin_transaction():
