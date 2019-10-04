e327e13aae7857795776b68f80e8e8de7f17a413
Adamos Kyriakou <somada141@gmail.com>
2019-10-04 12:51:19 +0930

    Bumped version and updated `CHANGELOG.md`.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='ct_ingester',
-    version='0.15.1',
+    version='0.16.0',
     description="ClinicalTrials XML dump parser and SQL ingester.",
     long_description=readme + '\n\n' + history,
     author="Adamos Kyriakou",
